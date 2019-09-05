4bd4425a919063eb18644f78c384b1fe7f34736f
Carla Morral Martin <31885862+carlaMorral@users.noreply.github.com>
2019-09-05 15:33:36 +0200

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,13 +1,3 @@
-/*
- * Estructura de Dades. Laboratori.
- * Pràctica 3: Arbres binaris
- */
-
-/* 
- * File:    BinarySearchTree.h
- * Authors: Carla Morral, Oscar Lacasta
- */
-
 #ifndef BINARYSEARCHTREE_H
 #define BINARYSEARCHTREE_H
 
