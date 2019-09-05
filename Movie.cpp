0796f6882ed049e92fcd221124b72f6cf877d5e9
Carla Morral Martin <31885862+carlaMorral@users.noreply.github.com>
2019-09-05 15:33:59 +0200

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -1,13 +1,3 @@
-/*
- * Estructura de Dades. Laboratori.
- * Pràctica 3: Arbres binaris
- */
-
-/* 
- * File:    Movie.cpp
- * Authors: Carla Morral, Oscar Lacasta
- */
-
 #include <iomanip>
 #include <string>
 #include <sstream>
